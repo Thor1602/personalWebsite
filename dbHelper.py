892f5d925a756b0c692d3cce893469591f514a56
thor1602 <thorbendhaenenstd@gmail.com>
2021-01-09 20:04:42 +0900

    Query processing is made more simple by combining it in 1 function, though there are still problems like adding a new user.

--- dbHelper.py
+++ dbHelper.py
@@ -1,82 +1,121 @@
 import sqlite3
 from sqlite3 import Error
 
-class User:
+class Main:
+    def execute_query(self, query_list, commit=False, fetchAll=False, fetchOne=False):
+        try:
+            conn = sqlite3.connect("mainsite.db")
+            c = conn.cursor()
+            result = None
+            if type(query_list) == str:
+                c.execute(query_list)
+            else:
+                for query in query_list:
+                    c.execute(query)
+            if commit:
+                conn.commit()
+            if fetchAll:
+                result = [row for row in c.fetchall()]
+            if fetchOne:
+                result = c.fetchone()
+        except Error as e:
+            print(e)
+        finally:
+            c.close()
+            conn.close()
+            return result
+
+    def read_database(self, db_name):
+        data = self.execute_query(query_list="SELECT * FROM " + db_name + "", fetchAll=True)
+        print(data)
+
+class User(Main):
     def __init__(self, username_arg, password_arg, remember_me_arg):
         self.username = username_arg
         self.password = password_arg
         self.remember_me = remember_me_arg
 
-    def add_user(self):
-        try:
-            conn = sqlite3.connect("mainsite.db")
-            c = conn.cursor()
-            c.execute('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, password TEXT NOT NULL, remember_me INTEGER NOT NULL)')
-            c.execute("INSERT INTO user (name, password, remember_me) VALUES (?,?,?)", (self.username, self.password, self.remember_me))
-            conn.commit()
-        except Error as e:
-            print(e)
+    def to_string(self):
+        return self.username + ", " + self.password + ", " + str(self.remember_me)+ "."
 
-        finally:
-            c.close()
-            conn.close()
+    def add_user(self):
+        query1 ="""CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, password TEXT NOT NULL, remember_me INTEGER NOT NULL)"""
+        query2 = "INSERT INTO user (name, password, remember_me) VALUES (?,?,?)", (self.username, self.password, self.remember_me)
+        query_list = [query1, query2]
+        self.execute_query(query2, commit=True)
 
     def del_user(self, name):
-        try:
-            conn = sqlite3.connect("mainsite.db")
-            c = conn.cursor()
-            c.execute('DELETE FROM user WHERE name = ' + name + '')
-            conn.commit()
-        except Error as e:
-            print(e)
+        self.execute_query(query_list='DELETE FROM user WHERE name = ' + name + '', commit=True)
 
-        finally:
-            c.close()
-            conn.close()
-
-    def read_database(self):
-        try:
-            conn = sqlite3.connect("mainsite.db")
-            c = conn.cursor()
-            print([x for x in c.execute("SELECT * FROM user")])
-        except Error as e:
-            print(e)
-
-        finally:
-            c.close()
-            conn.close()
     def check_user(self):
-        try:
-            conn = sqlite3.connect("mainsite.db")
-            c = conn.cursor()
-            c.execute("SELECT * FROM user WHERE name = '" + self.username + "'")
-            query = c.fetchone()
-            if query == None:
-                return False
+        query = "SELECT * FROM user WHERE name = '" + self.username + "'"
+        exec_query = self.execute_query(query_list=query, fetchOne=True)
+        if exec_query == None:
+            return False
+        else:
+            query_list = [x for x in exec_query]
+            if query_list[1] == self.username and query_list[2] == self.password:
+                return True
             else:
-                query_list = [x for x in query]
-                if query_list[1] == self.username and query_list[2] == self.password:
-                    return True
-                else:
-                    return False
+                return False
 
-        except Error as e:
-            print(e)
-        finally:
-            c.close()
-            conn.close()
+class Blog(Main):
+    def add_entry(self, title, description, paragraphlist, listImageID):
+        query1 ="""CREATE TABLE IF NOT EXISTS blogs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        date DATE NOT NULL,
+        title TEXT NOT NULL, 
+        description TEXT NOT NULL, 
+        paragraph1 TEXT NOT NULL,
+        paragraph2 TEXT NOT NULL,
+        paragraph3 TEXT NOT NULL,
+        paragraph4 TEXT NOT NULL,
+        paragraph5 TEXT NOT NULL,
+        paragraph6 TEXT NOT NULL,
+        paragraph7 TEXT NOT NULL,
+        paragraph8 TEXT NOT NULL,
+        paragraph9 TEXT NOT NULL,
+        paragraph10 TEXT NOT NULL,
+        paragraph11 TEXT NOT NULL,
+        paragraph12 TEXT NOT NULL,
+        listImageID TEXT NOT NULL)"""
+        query2 = "INSERT INTO user (name, password, remember_me) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", ()
+        query_list = [query1, query2]
+        self.execute_query(query_list=query_list, commit=True)
 
-class Blog:
+    def remove_entry(self):
+        pass
+    def update_entry(self):
+        pass
+
+
+class Project(Main):
     pass
 
-class Project:
+class Tutorial(Main):
     pass
 
-class Tutorial:
+class Settings(Main):
     pass
 
-class Comment:
+class MediaUpload(Main):
+    table_name = "media_upload"
+    def add_entry(self, image_id, url):
+        query1 = """"""
+        query2 = ""
+        query_list = [query1, query2]
+        self.execute_query(query_list=query_list, commit=True)
+
+    def remove_entry(self, image_id):
+        self.execute_query(query_list='DELETE FROM ' + self.table_name + ' WHERE image_id = ' + image_id + '', commit=True)
+    def update_entry(self, image_id='', url=''):
+        query = "UPDATE table SET column_1 = new_value_1, column_2 = new_value_2 WHERE search_condition"
+        self.execute_query(query_list='UPDATE ' + self.table_name + ' SET image_id = ' + image_id + '', commit=True)
+
+
+
+class Comment(Main):
     pass
 
-class Info:
+class Info(Main):
     pass
--- testdb.py
+++ testdb.py
@@ -1,4 +1,6 @@
 import dbHelper
 
-user1 = dbHelper.User('thorben1602', "WPvX4NNn2rRTg56QW52V", True)
-print(user1.check_user())
+user1 = dbHelper.User('thorben1602', "WPvX4NNn2rRTg56QW52V", 1)
+user2 = dbHelper.User('sumin2502', "vX4N26QW5%%rRT2VWPng", 1)
+user2.add_user()
+user1.read_database('user')
